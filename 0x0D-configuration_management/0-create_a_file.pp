a190ff1f9beb53907368715ce38603d18eed6bac
Larry Madeo <lmadeo76@gmail.com>
2017-11-17 01:10:47 +0000

    first commit

---
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+file { '/tmp/holberton':
+  ensure   => 'file',
+  path     => '/tmp/holberton',
+  content  => 'I love Puppet',
+  mode     => '0744',
+  owner    => 'www-data',
+  group    => 'www-data'
+  }
