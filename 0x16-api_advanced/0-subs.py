5095f88def1d01a25aa3af29ba8ccc9d4394448d
Larry Madeo <lmadeo76@gmail.com>
2018-01-08 13:01:21 -0800

    first commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''module: 0-subs
+'''
+
+import pprint
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''number_of_subscribers: hit reddit api and
+    returns: int: number of subscribers for a particular subreddit
+    if subreddit does not exist, return 0
+    '''
+
+    url = "https://reddit.com/r/" + subreddit + "/about/.json"
+    headers = {"User-Agent":  "larry-agent"}
+    r = requests.get(url, headers=headers)
+
+    subs = r.json().get("data").get("subscribers")
+    subscribers = subs if subs is not None else 0
+    return(subscribers)
